3e20ad03836db06310d0237538175428d2be0d7d
Lizbeth-girl <saezlizbethe6@gamail.com>
2024-11-05 09:31:59 -0500

    Jir

---
+++ src/main/java/ec/edu/espoch/jir/Jir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.jir;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Jir {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
