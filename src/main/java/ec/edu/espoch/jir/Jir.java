57bd82ae2a903a859369186294c083f69430cd99
Lizbeth-girl <saezlizbethe6@gamail.com>
2024-11-05 09:34:10 -0500

    Jir x2

--- src/main/java/ec/edu/espoch/jir/Jir.java
+++ src/main/java/ec/edu/espoch/jir/Jir.java
@@ -12,5 +12,7 @@ public class Jir {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Soy Jirrr");
+        System.out.println("Bienvenido");
     }
 }
